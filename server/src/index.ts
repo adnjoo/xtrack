a57eea70158deb9f4926f3a73b3dd9f75a45393d
Andrew Njoo <adnjoo@gmail.com>
2024-01-20 19:09:25 -0800

    upsert

--- server/src/index.ts
+++ server/src/index.ts
@@ -28,19 +28,54 @@ app.get("/expenses", async (req, res) => {
   }
 });
 
-app.post("/expenses/new", async (req, res) => {
+app.post("/expenses/upsert", async (req, res) => {
   try {
-    const { description, amount, date } = req.body;
+    const { description, amount, date, id } = req.body;
 
-    const newExpense = await prisma.expense.create({
-      data: {
-        description,
-        amount,
-        date,
+    const existingExpense = await prisma.expense.findUnique({
+      where: {
+        id: Number(id),
       },
     });
 
-    res.status(201).send(newExpense);
+    if (existingExpense) {
+      const updatedExpense = await prisma.expense.update({
+        where: {
+          id: Number(id),
+        },
+        data: {
+          description,
+          amount,
+          date,
+        },
+      });
+      res.send(updatedExpense);
+      return;
+    } else {
+      const newExpense = await prisma.expense.create({
+        data: {
+          description,
+          amount,
+          date,
+        },
+      });
+      res.send(newExpense);
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.delete("/expenses/delete/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedExpense = await prisma.expense.delete({
+      where: {
+        id: Number(id),
+      },
+    });
+    res.send(deletedExpense);
   } catch (error) {
     console.error("Error:", error);
     res.status(500).send("Internal Server Error");
