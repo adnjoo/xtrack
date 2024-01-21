d084ace9c0e53e44e6bdcad9f179d7bce33db33b
Andrew Njoo <adnjoo@gmail.com>
2024-01-20 20:41:18 -0800

    cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -34,6 +34,13 @@ app.get("/expenses", ClerkExpressWithAuth({}), getExpenses);
 app.post("/expenses/upsert", upsertExpense);
 app.delete("/expenses/delete/:id", deleteExpense);
 
+// CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
