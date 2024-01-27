b71861c0478fadcd0934e8fa597f395e9555672f
Andrew Njoo <adnjoo@gmail.com>
2024-01-27 10:07:07 -0800

    queryClientConfig

--- next/src/app/components/Providers.tsx
+++ next/src/app/components/Providers.tsx
@@ -7,7 +7,7 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { queryClientConfig } from '@/app/lib/queryClient';
 
 const Providers = ({ children }: any) => {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(() => new QueryClient(queryClientConfig));
 
   return (
     <QueryClientProvider client={queryClient}>
