bd4593eefebefabed112a595027e50b1cb612674
Andrew Njoo <adnjoo@gmail.com>
2024-01-27 21:12:17 -0800

    @ alias

--- expo/App.tsx
+++ expo/App.tsx
@@ -1,26 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Button } from 'react-native';
-import axios from 'axios';
-import React from 'react';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Button } from "react-native";
+import axios from "axios";
+import React from "react";
+
+import { Test } from "@/components/Test";
 
 export default function App() {
   const [counter, setCounter] = React.useState(1);
-  const [data, setData] = React.useState<any>('');
+  const [data, setData] = React.useState<any>("");
   React.useEffect(() => {
-    axios.get(`https://jsonplaceholder.typicode.com/posts/${counter}`).then((res) => {
-      setData(res.data);
-    });
+    axios
+      .get(`https://jsonplaceholder.typicode.com/posts/${counter}`)
+      .then((res) => {
+        setData(res.data);
+      });
   }, [counter]);
 
   const handleClick = () => {
     setCounter(counter + 1);
-  }
+  };
 
   return (
     <View style={styles.container}>
       <Text>{data.title}</Text>
       <Text>{data.body}</Text>
+      <Test />
       <StatusBar style="auto" />
       <Button title="Click" onPress={handleClick} />
     </View>
@@ -30,8 +35,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
