f148254ad4b7130f634c8b11d3b66f4bbe1ebb00
Andrew Njoo <adnjoo@gmail.com>
2024-02-02 09:29:28 -0800

    hook

--- next/src/app/components/MySpeedDial.tsx
+++ next/src/app/components/MySpeedDial.tsx
@@ -7,23 +7,37 @@ import Draggable from 'react-draggable';
 
 import MyModal from '@/app/components/MyModal';
 import ExpenseForm from '@/app/components/ExpenseForm';
+import { useCheckMobileScreen } from '@/app/lib/hooks';
+
+const AddExpenseButton = ({ setModalOpen }: { setModalOpen: any }) => {
+  return (
+    <Button onClick={() => setModalOpen(true)} className='rounded-t-none'>
+      <Tooltip content='Add Expense' placement='left'>
+        <BsPlusCircleFill className='h-6 w-6' />
+      </Tooltip>
+    </Button>
+  );
+};
 
 export default function MySpeedDial() {
   const [modalOpen, setModalOpen] = React.useState(false);
+  const isMobile = useCheckMobileScreen();
 
   return (
     <>
       {/* Speed dial */}
-      <Draggable>
+      {isMobile ? (
         <div className='fixed bottom-4 right-4'>
-          <div className='max-h-[10px] rounded-t-lg bg-gray-800'>&nbsp;</div>
-          <Button onClick={() => setModalOpen(true)} className='rounded-t-none'>
-            <Tooltip content='Add Expense' placement='left'>
-              <BsPlusCircleFill className='h-6 w-6' />
-            </Tooltip>
-          </Button>
+          <AddExpenseButton setModalOpen={setModalOpen} />
         </div>
-      </Draggable>
+      ) : (
+        <Draggable>
+          <div className='fixed bottom-4 right-4'>
+            <div className='max-h-[10px] rounded-t-lg bg-gray-800'>&nbsp;</div>
+            <AddExpenseButton setModalOpen={setModalOpen} />
+          </div>
+        </Draggable>
+      )}
 
       {/* Modal */}
       <MyModal isOpen={modalOpen} setIsOpen={setModalOpen}>
