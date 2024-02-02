441e64ff6b0049552495dbc9ef6e5a25e93cfbbd
Andrew Njoo <adnjoo@gmail.com>
2024-02-02 07:35:57 -0800

    🐞: add expense blocking footer links (#82)

--- next/src/app/components/MySpeedDial.tsx
+++ next/src/app/components/MySpeedDial.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import { Button, Tooltip } from 'flowbite-react';
 import { BsPlusCircleFill } from 'react-icons/bs';
+import Draggable from 'react-draggable';
 
 import MyModal from '@/app/components/MyModal';
 import ExpenseForm from '@/app/components/ExpenseForm';
@@ -13,14 +14,16 @@ export default function MySpeedDial() {
   return (
     <>
       {/* Speed dial */}
-      <Button
-        className='fixed bottom-4 right-4'
-        onClick={() => setModalOpen(true)}
-      >
-        <Tooltip content='Add Expense' placement='left'>
-          <BsPlusCircleFill className='h-6 w-6' />
-        </Tooltip>
-      </Button>
+      <Draggable>
+        <div className='fixed bottom-4 right-4'>
+          <div className='max-h-[10px] rounded-t-lg bg-gray-800'>&nbsp;</div>
+          <Button onClick={() => setModalOpen(true)} className='rounded-t-none'>
+            <Tooltip content='Add Expense' placement='left'>
+              <BsPlusCircleFill className='h-6 w-6' />
+            </Tooltip>
+          </Button>
+        </div>
+      </Draggable>
 
       {/* Modal */}
       <MyModal isOpen={modalOpen} setIsOpen={setModalOpen}>
