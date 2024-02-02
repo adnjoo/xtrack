aa9b43d2fd0cf34f4ce28bb64be7428d4b511f83
Andrew Njoo <adnjoo@gmail.com>
2024-02-02 09:37:42 -0800

    🐞: add expense not working on next mobile (#85)
    * hook
    * refactor

--- next/src/app/components/MySpeedDial.tsx
+++ next/src/app/components/MySpeedDial.tsx
@@ -7,23 +7,46 @@ import Draggable from 'react-draggable';
 
 import MyModal from '@/app/components/MyModal';
 import ExpenseForm from '@/app/components/ExpenseForm';
+import { useCheckMobileScreen } from '@/app/lib/hooks';
+
+const AddExpenseButton = ({
+  setModalOpen,
+  isMobile = false,
+}: {
+  setModalOpen: any;
+  isMobile?: boolean;
+}) => {
+  return (
+    <div className='fixed bottom-4 right-4'>
+      {!isMobile && (
+        <div className='max-h-[10px] rounded-t-lg bg-gray-800'>&nbsp;</div>
+      )}
+      <Button
+        onClick={() => setModalOpen(true)}
+        className={!isMobile ? 'rounded-t-none' : ''}
+      >
+        <Tooltip content='Add Expense' placement='left'>
+          <BsPlusCircleFill className='h-6 w-6' />
+        </Tooltip>
+      </Button>
+    </div>
+  );
+};
 
 export default function MySpeedDial() {
   const [modalOpen, setModalOpen] = React.useState(false);
+  const isMobile = useCheckMobileScreen();
 
   return (
     <>
       {/* Speed dial */}
-      <Draggable>
-        <div className='fixed bottom-4 right-4'>
-          <div className='max-h-[10px] rounded-t-lg bg-gray-800'>&nbsp;</div>
-          <Button onClick={() => setModalOpen(true)} className='rounded-t-none'>
-            <Tooltip content='Add Expense' placement='left'>
-              <BsPlusCircleFill className='h-6 w-6' />
-            </Tooltip>
-          </Button>
-        </div>
-      </Draggable>
+      {isMobile ? (
+        <AddExpenseButton setModalOpen={setModalOpen} isMobile />
+      ) : (
+        <Draggable>
+          <AddExpenseButton setModalOpen={setModalOpen} />
+        </Draggable>
+      )}
 
       {/* Modal */}
       <MyModal isOpen={modalOpen} setIsOpen={setModalOpen}>
