eba1937b30461703cc90d0fb2177c716b4a41c18
Andrew Njoo <adnjoo@gmail.com>
2024-02-04 07:01:03 -0800

    next: unit testing (#91)
    * config
    * coverage

--- next/src/app/lib/utils.ts
+++ next/src/app/lib/utils.ts
@@ -3,7 +3,13 @@ import { DateValueType } from 'react-tailwindcss-datepicker';
 export const API_URL =
   process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4001';
 
-export const classNames = (...classes: string[]) => {
+/**
+ * Returns a string of space-separated class names, filtering out any undefined or empty strings.
+ *
+ * @param {string | undefined} classes - The list of class names to be concatenated.
+ * @return {string} The concatenated class names.
+ */
+export const classNames = (...classes: (string | undefined)[]) => {
   return classes.filter(Boolean).join(' ');
 };
 
