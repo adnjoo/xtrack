7b5735f19790a4daaeb4a2ed4536d85fb0d40ed3
Andrew Njoo <adnjoo@gmail.com>
2024-03-03 19:50:32 -0800

    line chart

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -2,6 +2,7 @@ import { auth } from '@clerk/nextjs';
 
 import MySpeedDial from '@/app/components/MySpeedDial';
 import ExpenseTable from '@/app/components/ExpenseTable';
+import Tremor from '@/app/components/Tremor';
 
 export default function Home() {
   const { userId } = auth();
@@ -11,6 +12,7 @@ export default function Home() {
       <section className='mx-auto mt-12 max-w-4xl overflow-x-auto p-1 sm:p-4'>
         <ExpenseTable />
       </section>
+      <Tremor />
       {userId && <MySpeedDial />}
     </>
   );
