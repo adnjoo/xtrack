d700c7a68a7c3a8b7145355c843111997c491b9f
Andrew Njoo <adnjoo@gmail.com>
2024-03-08 06:56:50 -0800

    blog

--- next/src/app/(marketing)/blog/page.tsx
+++ next/src/app/(marketing)/blog/page.tsx
@@ -1,7 +1,54 @@
+const posts = [
+  {
+    id: 1,
+    title: 'Why track expenses',
+    href: '#',
+    description:
+      'Get a better understanding of where your money is being spent.',
+    date: 'Mar 8, 2024',
+    datetime: '2024-03-08',
+  },
+  // More posts...
+];
+
 export default function Blog() {
   return (
-    <div>
-      <h1>Blog</h1>
+    <div className='bg-white py-24 sm:py-32'>
+      <div className='mx-auto max-w-7xl px-6 lg:px-8'>
+        <div className='mx-auto max-w-2xl lg:mx-0'>
+          <h2 className='text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
+            From the blog
+          </h2>
+          <p className='mt-2 text-lg leading-8 text-gray-600'>
+            Learn how to better maanage your finances!
+          </p>
+        </div>
+        <div className='mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3'>
+          {posts.map((post) => (
+            <article
+              key={post.id}
+              className='flex max-w-xl flex-col items-start justify-between'
+            >
+              <div className='flex items-center gap-x-4 text-xs'>
+                <time dateTime={post.datetime} className='text-gray-500'>
+                  {post.date}
+                </time>
+              </div>
+              <div className='group relative'>
+                <h3 className='mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600'>
+                  <a href={post.href}>
+                    <span className='absolute inset-0' />
+                    {post.title}
+                  </a>
+                </h3>
+                <p className='mt-5 line-clamp-3 text-sm leading-6 text-gray-600'>
+                  {post.description}
+                </p>
+              </div>
+            </article>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
