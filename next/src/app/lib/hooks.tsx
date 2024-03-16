7fdc80361596ddf96c8dce58414593c204c5150e
Andrew Njoo <adnjoo@gmail.com>
2024-03-15 21:00:41 -0700

    🐞: window is not defined

--- next/src/app/lib/hooks.tsx
+++ next/src/app/lib/hooks.tsx
@@ -6,11 +6,17 @@ import React, { useEffect, useState } from 'react';
  * @return {boolean}
  */
 export const useCheckMobileScreen = () => {
+  if (typeof window === 'undefined') {
+    return false;
+  }
+  
+  // eslint-disable-next-line react-hooks/rules-of-hooks
   const [width, setWidth] = useState(window.innerWidth);
   const handleWindowSizeChange = () => {
     setWidth(window.innerWidth);
   };
 
+  // eslint-disable-next-line react-hooks/rules-of-hooks
   useEffect(() => {
     window.addEventListener('resize', handleWindowSizeChange);
     return () => {
