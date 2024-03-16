aed91fcf1dac5e4b414f277bd2faacb4650dcc75
Andrew Njoo <adnjoo@gmail.com>
2024-03-16 07:41:36 -0700

    aggregate migrations

--- server/prisma/migrations/20240315063021_/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "Subscription" ALTER COLUMN "dateStarted" DROP NOT NULL,
-ALTER COLUMN "dateEnded" DROP NOT NULL;
--- server/prisma/migrations/20240316144118_/migration.sql
+++ server/prisma/migrations/20240316144118_/migration.sql
@@ -12,6 +12,21 @@ CREATE TABLE "Expense" (
 );
 
 -- CreateTable
+CREATE TABLE "Subscription" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "amount" DECIMAL(65,30) NOT NULL,
+    "category" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "dateAdded" TIMESTAMP(3) NOT NULL,
+    "dateStarted" TIMESTAMP(3),
+    "dateEnded" TIMESTAMP(3),
+    "clerkUserId" TEXT,
+
+    CONSTRAINT "Subscription_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "ClerkUser" (
     "id" TEXT NOT NULL,
 
@@ -20,3 +35,6 @@ CREATE TABLE "ClerkUser" (
 
 -- AddForeignKey
 ALTER TABLE "Expense" ADD CONSTRAINT "Expense_clerkUserId_fkey" FOREIGN KEY ("clerkUserId") REFERENCES "ClerkUser"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Subscription" ADD CONSTRAINT "Subscription_clerkUserId_fkey" FOREIGN KEY ("clerkUserId") REFERENCES "ClerkUser"("id") ON DELETE CASCADE ON UPDATE CASCADE;
