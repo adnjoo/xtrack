b68ab0ce2be979986a3add8311c232dc56ddf49b
Andrew Njoo <adnjoo@gmail.com>
2024-03-17 14:52:39 -0700

    130: bug fix

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,4 +1,4 @@
-import { auth } from '@clerk/nextjs';
+import { SignInButton, auth } from '@clerk/nextjs';
 
 import MySpeedDial from '@/app/components/organisms/MySpeedDial';
 import TabsHero from '@/app/components/organisms/TabsHero';
@@ -19,12 +19,11 @@ export default function Home() {
               Supercharge your net worth by tracking your expenses.
             </p>
             <div className='mt-10 flex items-center justify-center gap-x-6'>
-              <a
-                href='/signin'
-                className='rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
-              >
-                Get started
-              </a>
+              <SignInButton>
+                <button className='rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>
+                  Get started
+                </button>
+              </SignInButton>
               <a
                 href='/about'
                 className='text-sm font-semibold leading-6 text-gray-900'
