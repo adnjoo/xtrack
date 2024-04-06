72cdf8458e582727d829a7e41eba71f8ef2d6796
Andrew Njoo <adnjoo@gmail.com>
2024-04-06 15:27:40 -0700

    signin button (#150)

--- web/src/app/components/organisms/MyNavbar.tsx
+++ web/src/app/components/organisms/MyNavbar.tsx
@@ -10,14 +10,14 @@ import { APP_NAME, PAGES } from '@/app/lib/constants';
 export default function MyNavbar() {
   const showPages = Object.values(PAGES).filter((page) => page.menu);
   return (
-    <Disclosure as='nav' className='bg-gray-800'>
+    <Disclosure as='nav' className='bg-white border-b border-black'>
       {({ open }) => (
         <>
           <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
             <div className='relative flex h-16 items-center justify-between'>
               <div className='absolute inset-y-0 left-0 flex items-center sm:hidden'>
                 {/* Mobile menu button*/}
-                <Disclosure.Button className='relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'>
+                <Disclosure.Button className='relative inline-flex items-center justify-center rounded-md p-2 text-black hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'>
                   <span className='absolute -inset-0.5' />
                   <span className='sr-only'>Open main menu</span>
                   {open ? (
@@ -27,11 +27,12 @@ export default function MyNavbar() {
                   )}
                 </Disclosure.Button>
               </div>
+
               <div className='flex flex-1 items-center justify-center sm:items-stretch sm:justify-start'>
                 <Link
                   href={PAGES.HOME.href}
                   passHref
-                  className='flex flex-shrink-0 items-center text-white'
+                  className='flex flex-shrink-0 items-center text-black'
                 >
                   <img src='/logo.svg' className='mr-3 h-9' alt='XT Logo' />
                   <span className='hidden sm:flex'>{APP_NAME}</span>
@@ -42,7 +43,7 @@ export default function MyNavbar() {
                       <Link
                         key={item.label}
                         href={item.href}
-                        className='rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white'
+                        className='rounded-md px-3 py-2 text-sm font-medium text-black hover:bg-gray-700 hover:text-white transition-all duration-300'
                       >
                         {item.label}
                       </Link>
@@ -50,6 +51,7 @@ export default function MyNavbar() {
                   </div>
                 </div>
               </div>
+
               <div className='absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0'>
                 {/* Profile */}
                 <SignedIn>
@@ -57,7 +59,9 @@ export default function MyNavbar() {
                 </SignedIn>
                 <SignedOut>
                   <SignInButton>
-                    <button className='text-white'>Sign in</button>
+                    <button className='h-[42px] w-[100px] rounded-lg bg-[#3758F9] text-white transition-all duration-300 hover:bg-blue-800 hover:shadow-lg'>
+                      Sign In
+                    </button>
                   </SignInButton>
                 </SignedOut>
               </div>
@@ -71,7 +75,7 @@ export default function MyNavbar() {
                   key={item.label}
                   as={Link}
                   href={item.href}
-                  className='block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white'
+                  className='block rounded-md px-3 py-2 text-base font-medium text-black hover:bg-gray-700 hover:text-white transition-all duration-300'
                 >
                   {item.label}
                 </Disclosure.Button>
