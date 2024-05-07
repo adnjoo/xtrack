3e04466aa03400b6c5044e4aba34586b607a2db7
Andrew Njoo <adnjoo@gmail.com>
2024-05-06 21:01:04 -0700

    routing

--- mobile/app/about.tsx
+++ mobile/app/about.tsx
@@ -1,9 +1,11 @@
 import { View, Text } from "react-native";
+import { Link } from "expo-router";
 
 const About = () => {
   return (
     <View>
       <Text>About</Text>
+      <Link href="/">Home</Link>
     </View>
   );
 };
