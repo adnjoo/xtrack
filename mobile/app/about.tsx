c672078dfa46f3aa88db90b14b5674638dd98a98
Andrew Njoo <adnjoo@gmail.com>
2024-05-06 21:04:18 -0700

    @expo/html-elements

--- mobile/app/about.tsx
+++ mobile/app/about.tsx
@@ -1,13 +1,44 @@
-import { View, Text } from "react-native";
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 import { Link } from "expo-router";
+import { A } from "@expo/html-elements";
 
 const About = () => {
   return (
-    <View>
-      <Text>About</Text>
-      <Link href="/">Home</Link>
+    <View style={styles.container}>
+      <Text style={styles.title}>About</Text>
+      <Text style={styles.description}>
+        XTrack was created out of a need to better track finances, by{" "}
+        <A href="https://bamboostud.io">Team Bamboo 🎋</A>.
+      </Text>
+      <Link href="/" style={styles.link}>
+        Go to Home
+      </Link>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  description: {
+    fontSize: 16,
+    textAlign: "center",
+    marginBottom: 20,
+  },
+  link: {
+    color: "blue",
+    fontSize: 18,
+  },
+});
 
 export default About;
