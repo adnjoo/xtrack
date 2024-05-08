ee1227b7b4e1dc1c2ae61bf99a5e64bd6de0c088
Andrew Njoo <adnjoo@gmail.com>
2024-05-07 20:15:26 -0700

    @expo/vector-icons

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -1,10 +1,48 @@
+import React from "react";
 import { Tabs } from "expo-router";
+import { StyleSheet } from "react-native";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 export default function TabLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" />
-      <Tabs.Screen name="about" />
+    <Tabs
+      screenOptions={{
+        tabBarActiveBackgroundColor: "#ffffff",
+        tabBarInactiveBackgroundColor: "white",
+        tabBarLabelStyle: styles.tabLabel,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Spending",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons size={24} name="attach-money" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: "Settings",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="cog" size={24} color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
+
+const styles = StyleSheet.create({
+  tabLabel: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "black",
+  },
+  tabIndicator: {
+    backgroundColor: "blue",
+    height: 3,
+  },
+});
