4273fa7d6f93c353ee9363b02dacf2675fe40268
Andrew Njoo <adnjoo@gmail.com>
2024-05-11 09:17:03 -0700

    sql

---
+++ web/supabase/2-expenses.sql
@@ -0,0 +1,12 @@
+CREATE TABLE expenses (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255),
+    price VARCHAR(255) DEFAULT '0',
+    category VARCHAR(255),
+    date VARCHAR(255) NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    user_id UUID NOT NULL REFERENCES profiles(id) ON DELETE CASCADE,
+    nameHash VARCHAR(255)
+);
