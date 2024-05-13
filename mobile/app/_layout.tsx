548736f6406724d38a84ff0053794ac31fd3f888
Andrew Njoo <adnjoo@gmail.com>
2024-05-12 23:39:05 -0700

    useRouter (#247)

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -33,6 +33,7 @@ export default function RootLayout() {
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+        <Stack.Screen name='modal' options={{ presentation: 'modal', title: 'Add expense' }} />
         <Stack.Screen name='+not-found' />
       </Stack>
     </ThemeProvider>
