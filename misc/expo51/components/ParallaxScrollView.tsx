3d43279dab337d1fab74451b3da4185417fdb459
Andrew Njoo <adnjoo@gmail.com>
2024-05-12 19:09:40 -0700

    supabase expo51 (#239)
    * add files
    * npx prettier . --write

--- misc/expo51/components/ParallaxScrollView.tsx
+++ misc/expo51/components/ParallaxScrollView.tsx
@@ -1,4 +1,3 @@
-import type { PropsWithChildren, ReactElement } from 'react';
 import { StyleSheet, useColorScheme } from 'react-native';
 import Animated, {
   interpolate,
@@ -6,7 +5,7 @@ import Animated, {
   useAnimatedStyle,
   useScrollViewOffset,
 } from 'react-native-reanimated';
-
+import type { PropsWithChildren, ReactElement } from 'react';
 import { ThemedView } from '@/components/ThemedView';
 
 const HEADER_HEIGHT = 250;
@@ -36,7 +35,11 @@ export default function ParallaxScrollView({
           ),
         },
         {
-          scale: interpolate(scrollOffset.value, [-HEADER_HEIGHT, 0, HEADER_HEIGHT], [2, 1, 1]),
+          scale: interpolate(
+            scrollOffset.value,
+            [-HEADER_HEIGHT, 0, HEADER_HEIGHT],
+            [2, 1, 1]
+          ),
         },
       ],
     };
@@ -50,7 +53,8 @@ export default function ParallaxScrollView({
             styles.header,
             { backgroundColor: headerBackgroundColor[colorScheme] },
             headerAnimatedStyle,
-          ]}>
+          ]}
+        >
           {headerImage}
         </Animated.View>
         <ThemedView style={styles.content}>{children}</ThemedView>
