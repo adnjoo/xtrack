2943f266939c535fbfa7f6386c2f9152e8665b47
Andrew Njoo <adnjoo@gmail.com>
2024-05-25 20:53:39 -0700

    gap

--- web/app/notes/page.tsx
+++ web/app/notes/page.tsx
@@ -6,8 +6,10 @@ export default async function Page() {
   const { data: notes } = await supabase.from('notes').select();
   const points = notes?.map((note) => note.done).reduce((a, b) => a + b, 0);
   return (
-    <div>
-      {notes?.map((note) => <Note note={note} />)}
+    <div className='mt-12'>
+      <div className='gap-4 flex flex-col'>
+        {notes?.map((note) => <Note note={note} />)}
+      </div>
       <div>total points: {points}</div>
       <div>make note</div>
     </div>
