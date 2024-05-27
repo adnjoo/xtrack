7cde79cfb18231b247220b908a130bddee1025c5
Andrew Njoo <adnjoo@gmail.com>
2024-05-27 08:19:44 -0700

    post it (#263)
    * points

---
+++ web/app/(protected)/notes/note.tsx
@@ -0,0 +1,112 @@
+'use client';
+
+import { useState } from 'react';
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { cn } from '@/lib/utils';
+import { createClient } from '@/utils/supabase/client';
+
+export const Note = ({ note }: { note: any }) => {
+  const supabase = createClient();
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTitle, setEditedTitle] = useState(note.title);
+  const [isDone, setIsDone] = useState(note.done);
+
+  const handleDelete = async (id: number) => {
+    if (!window.confirm('Are you sure you want to delete this note?')) return;
+    await supabase.from('notes').delete().eq('id', id);
+  };
+
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+
+  const handleSave = async () => {
+    await supabase
+      .from('notes')
+      .update({ title: editedTitle })
+      .eq('id', note.id);
+    setIsEditing(false);
+  };
+
+  const handleCancel = () => {
+    setEditedTitle(note.title);
+    setIsEditing(false);
+  };
+
+  const handleToggleDone = async () => {
+    const updatedStatus = !isDone;
+    await supabase
+      .from('notes')
+      .update({ done: updatedStatus })
+      .eq('id', note.id);
+    setIsDone(updatedStatus);
+  };
+
+  return (
+    <Card className='max-w-[300px]'>
+      <CardHeader>
+        <CardTitle className='flex justify-between items-center'>
+          {isEditing ? (
+            <Input
+              type='text'
+              value={editedTitle}
+              onChange={(e) => setEditedTitle(e.target.value)}
+              className='text-black'
+            />
+          ) : (
+            <>
+              <span
+                className={cn('cursor-pointer', isDone && 'line-through')}
+                onClick={handleToggleDone}
+              >
+                {note.title}
+              </span>
+              <span className='text-xs'>
+                {new Date(note.updated_at).toLocaleString('en-US', {
+                  year: 'numeric',
+                  month: '2-digit',
+                  day: '2-digit',
+                })}
+              </span>
+            </>
+          )}
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <CardDescription>
+          {/* Additional note details can go here */}
+        </CardDescription>
+      </CardContent>
+      <CardFooter className='flex gap-4'>
+        {isEditing ? (
+          <>
+            <Button variant='outline' onClick={handleSave}>
+              Save
+            </Button>
+            <Button variant='outline' onClick={handleCancel}>
+              Cancel
+            </Button>
+          </>
+        ) : (
+          <>
+            <Button variant='outline' onClick={handleEdit}>
+              Edit
+            </Button>
+            <Button variant='outline' onClick={() => handleDelete(note.id)}>
+              Delete
+            </Button>
+          </>
+        )}
+      </CardFooter>
+    </Card>
+  );
+};
---
+++ web/app/(protected)/notes/page.tsx
@@ -0,0 +1,18 @@
+import { createClient } from '@/utils/supabase/server';
+import { CreateNote } from './create';
+import { Note } from './note';
+
+export default async function Page() {
+  const supabase = createClient();
+  const { data: notes } = await supabase.from('notes').select().eq('done', false);
+  const points = notes?.map((note) => note.done).reduce((a, b) => a + b, 0);
+  return (
+    <div className='mt-12'>
+      <div className='gap-4 grid grid-cols-3 my-6'>
+        {notes?.map((note) => <Note note={note} />)}
+      </div>
+      <div>total points: {points}</div>
+      <CreateNote />
+    </div>
+  );
+}
