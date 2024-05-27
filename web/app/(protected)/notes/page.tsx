6c1de8d4750c7779953e4f125ca2a774fcc73ca1
Andrew Njoo <adnjoo@gmail.com>
2024-05-27 10:55:21 -0700

    stars (#265)
    * notes
    
    * points
    
    * style

--- web/app/(protected)/notes/page.tsx
+++ web/app/(protected)/notes/page.tsx
@@ -1,17 +1,18 @@
 import { createClient } from '@/utils/supabase/server';
+import { Badge } from "@/components/ui/badge"
 import { CreateNote } from './create';
 import { Note } from './note';
 
 export default async function Page() {
   const supabase = createClient();
-  const { data: notes } = await supabase.from('notes').select().eq('done', false);
+  const { data: notes } = await supabase.from('notes').select();
   const points = notes?.map((note) => note.done).reduce((a, b) => a + b, 0);
   return (
     <div className='mt-12'>
+      <Badge>⭐️ {points}</Badge>
       <div className='gap-4 grid grid-cols-3 my-6'>
-        {notes?.map((note) => <Note note={note} />)}
+        {notes?.filter((note) => !note.done).map((note) => <Note note={note} />)}
       </div>
-      <div>total points: {points}</div>
       <CreateNote />
     </div>
   );
