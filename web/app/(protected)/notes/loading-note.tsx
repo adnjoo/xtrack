87e357c4fca50f215b7284775f43cb8e59183041
Andrew Njoo <adnjoo@gmail.com>
2024-05-28 23:56:09 -0700

    cleanup

--- web/app/(protected)/notes/loading-note.tsx
+++
@@ -1,7 +0,0 @@
-import { Card } from '@/components/ui/card';
-
-export default function LoadingNote() {
-  return (
-    <Card className='h-[188px] w-[320px] animate-pulse bg-slate-300'></Card>
-  );
-}
