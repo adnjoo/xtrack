de67d653c657775745330d79ee98c70a97a4a921
Andrew Njoo <adnjoo@gmail.com>
2024-06-07 01:16:48 -0700

    swift

---
+++ iOS/L1 Demo/L1_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  L1_DemoApp.swift
+//  L1 Demo
+//
+//  Created by Andrew Njoo on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct L1_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
