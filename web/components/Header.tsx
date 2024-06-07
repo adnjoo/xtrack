ec0e7636495c30612bb426d3a4b404abc1d68770
Andrew Njoo <adnjoo@gmail.com>
2024-06-06 19:12:18 -0700

    copy

--- web/components/Header.tsx
+++ web/components/Header.tsx
@@ -3,7 +3,7 @@ export default function Header() {
     <div className='flex flex-col items-center gap-16'>
       <h1 className='sr-only'>XTrack</h1>
       <p className='mx-auto max-w-xl text-center text-3xl !leading-tight lg:text-4xl'>
-        The fastest way to GSD with{' '}
+        The fastest way to GTD with{' '}
         <span className='inline-block bg-gradient-to-r from-slate-700 via-gray-800 to-black bg-clip-text font-bold text-transparent'>
           Xtrack
         </span>
