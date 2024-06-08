c6c9309b0902b6a03e4bc7d0937df3258c1ab2d1
Andrew Njoo <adnjoo@gmail.com>
2024-06-08 00:16:18 -0700

    rnr

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,29 +1,89 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Theme, ThemeProvider } from '@react-navigation/native';
 import { Tabs } from 'expo-router';
+import { SplashScreen } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
+import { Platform } from 'react-native';
+import { NAV_THEME } from '~/lib/constants';
+import { useColorScheme } from '~/lib/useColorScheme';
 
 import '@/global.css';
 
-export default function Layout() {
+const LIGHT_THEME: Theme = {
+  dark: false,
+  colors: NAV_THEME.light,
+};
+const DARK_THEME: Theme = {
+  dark: true,
+  colors: NAV_THEME.dark,
+};
+
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from 'expo-router';
+
+// Prevent the splash screen from auto-hiding before getting the color scheme.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const { colorScheme, setColorScheme, isDarkColorScheme } = useColorScheme();
+  const [isColorSchemeLoaded, setIsColorSchemeLoaded] = React.useState(false);
+
+  React.useEffect(() => {
+    (async () => {
+      const theme = await AsyncStorage.getItem('theme');
+      if (Platform.OS === 'web') {
+        // Adds the background color to the html element to prevent white background on overscroll.
+        document.documentElement.classList.add('bg-background');
+      }
+      if (!theme) {
+        AsyncStorage.setItem('theme', colorScheme);
+        setIsColorSchemeLoaded(true);
+        return;
+      }
+      const colorTheme = theme === 'dark' ? 'dark' : 'light';
+      if (colorTheme !== colorScheme) {
+        setColorScheme(colorTheme);
+
+        setIsColorSchemeLoaded(true);
+        return;
+      }
+      setIsColorSchemeLoaded(true);
+    })().finally(() => {
+      SplashScreen.hideAsync();
+    });
+  }, []);
+
+  if (!isColorSchemeLoaded) {
+    return null;
+  }
+
   return (
-    <Tabs>
-      <Tabs.Screen
-        name='index'
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => (
-            <FontAwesome size={28} name='home' color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name='account'
-        options={{
-          title: 'Account',
-          tabBarIcon: ({ color }) => (
-            <FontAwesome size={28} name='user' color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+    <ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
+      <StatusBar style={isDarkColorScheme ? 'light' : 'dark'} />
+      <Tabs>
+        <Tabs.Screen
+          name='index'
+          options={{
+            title: 'Home',
+            tabBarIcon: ({ color }) => (
+              <FontAwesome size={28} name='home' color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name='account'
+          options={{
+            title: 'Account',
+            tabBarIcon: ({ color }) => (
+              <FontAwesome size={28} name='user' color={color} />
+            ),
+          }}
+        />
+      </Tabs>
+    </ThemeProvider>
   );
 }
