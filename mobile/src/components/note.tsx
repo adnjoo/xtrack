202d567c0f1349685ae8bf301fa4b76aa5a72de0
Andrew Njoo <adnjoo@gmail.com>
2024-06-08 16:00:47 -0700

    points

--- mobile/src/components/note.tsx
+++ mobile/src/components/note.tsx
@@ -17,6 +17,10 @@ export const Note = ({ note }: { note: any }) => {
   const { refetch: refetchNotes } = useQuery({
     queryKey: ['notes'],
   });
+  const { refetch: refetchPoints } = useQuery({
+    queryKey: ['points'],
+  });
+
   const handleTrashClick = () => {
     if (note.done) {
       Alert.alert(
@@ -50,30 +54,30 @@ export const Note = ({ note }: { note: any }) => {
   };
   const deleteNote = async () => {
     try {
-      const points = await supabase
-        .from('points')
-        .select()
-        .eq('user_id', note.user_id);
-      if (points.data && points.data.length > 0) {
-        await supabase
+      if (note.done) {
+        const points = await supabase
           .from('points')
-          .update({ points: points.data[0].points + 1 })
+          .select()
           .eq('user_id', note.user_id);
-      } else {
-        await supabase
-          .from('points')
-          .insert({ user_id: note.user_id, points: 1 });
+        if (points.data && points.data.length > 0) {
+          await supabase
+            .from('points')
+            .update({ points: points.data[0].points + 1 })
+            .eq('user_id', note.user_id);
+        } else {
+          await supabase
+            .from('points')
+            .insert({ user_id: note.user_id, points: 1 });
+        }
       }
-
       const { error } = await supabase.from('notes').delete().eq('id', note.id);
 
-      if (error) {
-        Alert.alert('Error', error.message);
-      }
+      if (error) throw error;
     } catch (error: any) {
       Alert.alert('Error', error.message);
     } finally {
       refetchNotes();
+      refetchPoints();
     }
   };
 
