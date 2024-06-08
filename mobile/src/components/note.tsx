ebd388447f7592ec0f5e2ff24a5fb515a946ae17
Andrew Njoo <adnjoo@gmail.com>
2024-06-08 16:10:28 -0700

    mobile update (#299)
    * points
    * edit

--- mobile/src/components/note.tsx
+++ mobile/src/components/note.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
-import { Trash } from 'lucide-react-native';
-import { Alert } from 'react-native';
+import { Edit, Save, Trash } from 'lucide-react-native';
+import { useState } from 'react';
+import { Alert, TextInput, View } from 'react-native';
 
 import { Button } from '@/src/components/ui/button';
 import {
@@ -17,6 +18,13 @@ export const Note = ({ note }: { note: any }) => {
   const { refetch: refetchNotes } = useQuery({
     queryKey: ['notes'],
   });
+  const { refetch: refetchPoints } = useQuery({
+    queryKey: ['points'],
+  });
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTitle, setEditedTitle] = useState(note.title);
+
   const handleTrashClick = () => {
     if (note.done) {
       Alert.alert(
@@ -48,32 +56,33 @@ export const Note = ({ note }: { note: any }) => {
       ]);
     }
   };
+
   const deleteNote = async () => {
     try {
-      const points = await supabase
-        .from('points')
-        .select()
-        .eq('user_id', note.user_id);
-      if (points.data && points.data.length > 0) {
-        await supabase
+      if (note.done) {
+        const points = await supabase
           .from('points')
-          .update({ points: points.data[0].points + 1 })
+          .select()
           .eq('user_id', note.user_id);
-      } else {
-        await supabase
-          .from('points')
-          .insert({ user_id: note.user_id, points: 1 });
+        if (points.data && points.data.length > 0) {
+          await supabase
+            .from('points')
+            .update({ points: points.data[0].points + 1 })
+            .eq('user_id', note.user_id);
+        } else {
+          await supabase
+            .from('points')
+            .insert({ user_id: note.user_id, points: 1 });
+        }
       }
-
       const { error } = await supabase.from('notes').delete().eq('id', note.id);
 
-      if (error) {
-        Alert.alert('Error', error.message);
-      }
+      if (error) throw error;
     } catch (error: any) {
       Alert.alert('Error', error.message);
     } finally {
       refetchNotes();
+      refetchPoints();
     }
   };
 
@@ -93,12 +102,37 @@ export const Note = ({ note }: { note: any }) => {
     }
   };
 
+  const saveEdit = async () => {
+    try {
+      const { error } = await supabase
+        .from('notes')
+        .update({ title: editedTitle, updated_at: new Date() })
+        .eq('id', note.id);
+      if (error) {
+        Alert.alert('Error', error.message);
+      } else {
+        setIsEditing(false);
+        refetchNotes();
+      }
+    } catch (error: any) {
+      Alert.alert('Error', error.message);
+    }
+  };
+
   return (
     <Card key={note.id} className='my-1'>
       <CardHeader>
-        <CardTitle className={note.done ? 'line-through' : ''}>
-          {note.title}
-        </CardTitle>
+        {isEditing ? (
+          <TextInput
+            value={editedTitle}
+            onChangeText={setEditedTitle}
+            style={{ borderBottomWidth: 1, borderColor: 'gray' }}
+          />
+        ) : (
+          <CardTitle className={note.done ? 'line-through' : ''}>
+            {note.title}
+          </CardTitle>
+        )}
         <CardDescription>
           {new Date(note.updated_at).toLocaleString('en-US', {
             year: 'numeric',
@@ -106,6 +140,17 @@ export const Note = ({ note }: { note: any }) => {
             day: 'numeric',
           })}
         </CardDescription>
+        <View className='absolute right-2 top-2'>
+          <Button className='bg-transparent' onPress={handleTrashClick}>
+            <Trash color='red' />
+          </Button>
+          <Button
+            className='bg-transparent'
+            onPress={isEditing ? saveEdit : () => setIsEditing(true)}
+          >
+            {isEditing ? <Save color='green' /> : <Edit color='blue' />}
+          </Button>
+        </View>
       </CardHeader>
       <CardContent>
         <Checkbox
@@ -113,12 +158,6 @@ export const Note = ({ note }: { note: any }) => {
           onCheckedChange={() => markDone(note.id, !note.done)}
         />
       </CardContent>
-      <Button
-        className='absolute right-2 top-2 bg-transparent'
-        onPress={() => handleTrashClick(note.done)}
-      >
-        <Trash color='red' />
-      </Button>
     </Card>
   );
 };
