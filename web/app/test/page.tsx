ee7343a27186431fb32a9e9df16be71d9729f4ce
Andrew Njoo <adnjoo@gmail.com>
2024-10-16 17:55:04 -0500

    register

--- web/app/test/page.tsx
+++ web/app/test/page.tsx
@@ -1,8 +1,18 @@
 import Login from '@/components/Login';
+import Register from '@/components/Register';
 
 export default function Page() {
-    return <div>
-        <h1 className='mt-12'>Test Page</h1>
+  return (
+    <div>
+      <h1 className='mt-12'>Test Page</h1>
+      <div className='my-24'>
+        Login
         <Login />
+      </div>
+      <div className='my-24'>
+        Register
+        <Register />
+      </div>
     </div>
+  );
 }
