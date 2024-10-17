ce06a9b2452353884833a1e117c4708096430611
Andrew Njoo <adnjoo@gmail.com>
2024-10-17 16:37:20 -0500

    s

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
-  }
+  devise_for :users
 
   namespace :api do
     get 'hello', to: 'hello#index'
