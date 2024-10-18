c38619542299391bb3d8a328e4cc761be6675222
Andrew Njoo <adnjoo@gmail.com>
2024-10-18 13:52:46 -0500

    wip

--- web/components/Navbar.tsx
+++ web/components/Navbar.tsx
@@ -9,18 +9,23 @@ export const Navbar = async () => {
   const {
     data: { user },
   } = await supabase.auth.getUser();
+
   return (
-    <header className='fixed z-50 left-0 top-0 h-16 w-full justify-center border-b border-b-foreground/10 bg-white'>
-      <div className='mx-auto flex max-w-7xl items-center justify-between px-6 py-3 text-sm lg:px-8'>
-        <Link href='/' className=''>
+    <header className='shadow-neo-brutal fixed left-0 top-0 z-50 h-20 w-full border-b-4 border-black bg-white px-6 py-3'>
+      <div className='mx-auto flex max-w-7xl items-center justify-between'>
+        <Link href='/' className='text-xl font-extrabold'>
           🫠 Xtrack
         </Link>
 
         <div className='flex items-center gap-4'>
           {user ? (
             <>
-              <Link href='/notes'>Notes</Link>
-              {/* <Link href='/protected'>Protected</Link> */}
+              <Link
+                href='/notes'
+                className='group inline-flex h-9 w-max items-center justify-center rounded-md border-2 border-black px-4 py-2 text-sm font-extrabold'
+              >
+                Notes
+              </Link>
             </>
           ) : null}
           <AuthButton />
