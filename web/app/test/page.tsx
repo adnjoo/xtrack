35fe640eb197ff3338e76947e967e4cacc72d92a
Andrew Njoo <adnjoo@gmail.com>
2024-10-18 21:04:04 -0500

    oganize

--- web/app/test/page.tsx
+++ web/app/test/page.tsx
@@ -1,5 +1,5 @@
-import Login from '@/components/Login';
-import Register from '@/components/Register';
+import Login from '@/components/auth/Login';
+import Register from '@/components/auth/Register';
 
 export default function Page() {
   return (
---
+++ web/components/ui/index.ts
@@ -0,0 +1,6 @@
+export * from './badge';
+export * from './button';
+export * from './card';
+export * from './checkbox';
+export * from './dialog';
+export * from './input';
