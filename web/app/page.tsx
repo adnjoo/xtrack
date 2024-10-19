e21f68170430c8a9f23d8dbfb16a85a949695b4f
Andrew Njoo <adnjoo@gmail.com>
2024-10-18 23:01:16 -0500

    web: navbar (#380)

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -3,7 +3,7 @@ import Header from '@/components/layout/Header';
 export default async function Index() {
   return (
     <div className='flex w-full flex-1 flex-col items-center gap-20'>
-      <div className='mt-8 flex max-w-4xl flex-1 flex-col gap-20 px-3 opacity-0 animate-in md:mt-16'>
+      <div className='mt-16 flex max-w-4xl flex-1 flex-col gap-20 px-3 opacity-0 animate-in md:mt-32'>
         <Header />
       </div>
     </div>
